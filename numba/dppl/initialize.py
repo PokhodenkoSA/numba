230c1c39ffd63bbea9e945ba6fcd177a6049374c
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-10-02 22:12:00 +0300

    Add logging for LLVM load_library_permanently.

--- numba/dppl/initialize.py
+++ numba/dppl/initialize.py
@@ -2,15 +2,22 @@ from __future__ import absolute_import, print_function
 import llvmlite.binding as ll
 import os
 from ctypes.util import find_library
+import logging
 
 
 def init_jit():
     from numba.dppl.dispatcher import DPPLDispatcher
     return DPPLDispatcher
 
+
 def initialize_all():
     from numba.core.registry import dispatcher_registry
     dispatcher_registry.ondemand['dppl'] = init_jit
 
-    ll.load_library_permanently(find_library('DPPLOpenCLInterface'))
-    ll.load_library_permanently(find_library('OpenCL'))
+    def load_library_permanently(name):
+        lib = find_library(name)
+        logging.info(f'LLVM:load_library_permanently: {lib}')
+        ll.load_library_permanently(lib)
+
+    load_library_permanently('DPPLOpenCLInterface')
+    load_library_permanently('OpenCL')
