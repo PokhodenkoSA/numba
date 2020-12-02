2f63307366616de55c2adbe3b1de2f29612566a6
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-12-02 03:31:28 -0600

    Remove numba-dppy

--- numba-dppy/conda-recipe/run_test.bat
+++
@@ -1,4 +0,0 @@
-python -m numba.runtests -b -v -m -- numba_dppy.tests
-IF %ERRORLEVEL% NEQ 0 exit /B 1
-
-exit /B 0
--- numba-dppy/conda-recipe/run_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-python -m numba.runtests -b -v -m -- numba_dppy.tests
-
-exit 0
